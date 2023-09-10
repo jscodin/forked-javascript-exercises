b6b7833f3684e4a0db76832a501657d1d0916dcd
jscodin <jascode9@gmail.com>
2023-09-10 13:19:27 +0100

    re-add 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
 const removeFromArray = function() {
 
+  /* Psuedocode
+  - function should accept an array 
+  and as many arguments as necessary: done
+  - function should then be able to know which arguments
+  need removing from array
+  - function should then remove the additonal arguments from
+  the array
+  - then return the new array without the removed arguments */
+  const filteredArr = array.filter(item => !args.includes(item))
+
+  console.log(array, filteredArr);
+  return filteredArr;
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
