f732e710f0f6cf86b1a439f2cd7cda5cd1d48cd4
jscodin <jascode9@gmail.com>
2023-09-16 23:31:10 +0100

    update code for task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,22 @@
 const sumAll = function(num1, num2) {
-  //psuedocode
-  // - get 2 integers from user input
-  // - add 1st integer to the next integer inbetween the 2 inputs and so on..
+
   let sum = 0;
 
-  for (let i = num1; i <= num2; i++) {
-    let num = i;
-    let total = sum + num;
-    console.log(num);
-    console.log(total);
-  };
 
-  // return sum;
+  if (num1 > num2) {
 
+
+    for (let i = num2; i <= num1; i++) {
+      sum = sum + i;
+      // console.log(sum);
+    } else {
+      for (let i = num1; i <= num2; i++) {
+        sum = sum + i;
+        //   console.log(sum);
+      }
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
