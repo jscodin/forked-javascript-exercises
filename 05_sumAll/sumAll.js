e4fd881fdd4e0b828497eba40c4a1e91766c5674
jscodin <jascode9@gmail.com>
2023-09-17 00:52:18 +0100

    add updated code for task 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,16 @@
 const sumAll = function(num1, num2) {
-
+  /*if (num1 || num2 < 0) {
+    return "ERROR";
+  }*/
   let sum = 0;
 
-
   if (num1 > num2) {
-
-
     for (let i = num2; i <= num1; i++) {
       sum = sum + i;
-      // console.log(sum);
-    } else {
-      for (let i = num1; i <= num2; i++) {
-        sum = sum + i;
-        //   console.log(sum);
-      }
+    }
+  } else {
+    for (let i = num1; i <= num2; i++) {
+      sum = sum + i;
     }
   }
   return sum;
