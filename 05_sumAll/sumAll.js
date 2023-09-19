1b54e4a9d8878ae77eea5c31cc85054fd7863db2
jscodin <jascode9@gmail.com>
2023-09-19 23:22:40 +0100

    Evening updated code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
 
+
   if (num1 > num2) {
     for (let i = num2; i <= num1; i++) {
       sum = sum + i;
@@ -10,13 +11,13 @@ const sumAll = function(num1, num2) {
       sum = sum + i;
     }
   }
-
-  if (num1 || num2 < 0) {
+  /* if (num1 || num2 < 0) {
     return "ERROR";
   }
-
+  else if (isNaN(num1 || num2)) {
+  } */
   return sum;
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
