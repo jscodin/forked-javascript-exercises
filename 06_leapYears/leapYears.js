1e0db6f394b89fea5d6aa67e01e6dca4effbc379
jscodin <jascode9@gmail.com>
2023-09-24 02:11:08 +0100

    upload answer for task 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+  /* psuedocode
+  - user inputs year
+  - function takes input and checks if it is divisable
+    by 4, if so potentially a leap year
+  - function also takes input and checks if it is divisable by 100, if so not a leap year, unless also
+  divisable by 400
+  */
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
 // Do not edit below this line
 module.exports = leapYears;
