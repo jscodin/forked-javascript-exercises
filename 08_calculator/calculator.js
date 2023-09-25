a9f0933dacf6fca75e837641245493d5cbf33bc7
jscodin <jascode9@gmail.com>
2023-09-25 21:51:41 +0100

    update code for task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
+
 };
 
 const sum = function() {
-	
-};
-
-const multiply = function() {
 
 };
 
-const power = function() {
-	
+const multiply = function(...args) {
+  let total = 1;
+  for (let i = 0; i < args.length; i++) {
+    total = total *= args[0];
+  }
+  return total;
+};
+
+const power = function(x, y) {
+  return x ** y;
+
 };
 
 const factorial = function() {
-	
+
 };
 
 // Do not edit below this line
