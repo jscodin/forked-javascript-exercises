4beab4e0352aec2e4d448c13d8d9411e8a4bd215
jscodin <jascode9@gmail.com>
2023-09-26 21:30:16 +0100

    Upload completed task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function(str) {
+
+  let reg = /[\W]/g; // used later to remove unwanted characters.
+  let lowRegStr = str.toLowerCase().replace(reg, ""); // replaces unwanted characters with no space
+  let reverseStr = lowRegStr.split("").reverse().join(""); // reverses the string, now with no spaces
+  return reverseStr === lowRegStr; // now both strings are in same format to be compared
 
 };
 
