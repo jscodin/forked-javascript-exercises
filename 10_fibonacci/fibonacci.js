d02860f242a86a7e73d8b1bd6a859b0bda1979d0
jscodin <jascode9@gmail.com>
2023-10-05 22:16:53 +0100

    Upload answer for task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,17 @@
 const fibonacci = function(num) {
 
-  let fib = 0;
+  let fib = [0, 1];
 
-  for (let i = 1; i <= num; i++) {
-
-
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    for (let i = 2; i <= num; i++) {
+      fib[i] = fib[i - 2] + fib[i - 1];
+      console.log(fib);
+      console.log(fib[num]);
+    }
   }
-
-
+  return fib[num];
 };
 
 // Do not edit below this line
