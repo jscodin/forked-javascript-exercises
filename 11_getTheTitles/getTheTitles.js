5ed8e3ce1f1f0efbf9d381d231a51eb08a2c4525
jscodin <jascode9@gmail.com>
2023-10-07 16:36:59 +0100

    upload answer for task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
 const getTheTitles = function(select) {
-  console.log(select[1].title);
+
+  /* let result = select.map(({ title }) => title);
+  console.log(result); */
+
+  const result = select.map((x) => x.title);
+  return result;
+
+
 };
 
 // Do not edit below this line
